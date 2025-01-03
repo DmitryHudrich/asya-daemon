44b20ca148c9cf02df188cdbcfff89fc245cbbf4
w1thluv <dimahudric@gmail.com>
2025-01-03 19:46:51 +0300

    allow dead code in stringify test

--- src/crates/macros/tests/stringify.rs
+++ src/crates/macros/tests/stringify.rs
@@ -1,24 +1,23 @@
 use macros::Stringify;
 
-
+#[allow(dead_code)]
 #[derive(Stringify)]
 enum TestEnum {
     First,
     Second(i32, usize),
-    Third {
-        name: String,
-        value: Option<i32>,
-    }
+    Third { name: String, value: Option<i32> },
 }
 
 #[test]
 fn simple_test() {
     println!("{}", TestEnum::stringify());
-    assert_eq!(TestEnum::stringify(), 
+    assert_eq!(
+        TestEnum::stringify(),
         "enum TestEnum {
     First,
     Second(i32, usize),
     Third { name: String, value: Option<i32> },
 }
-");
+"
+    );
 }
